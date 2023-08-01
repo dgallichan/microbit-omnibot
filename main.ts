99ea6ddaa604c85530284ee7aef1e51b716d6186
Daniel Gallichan <dgallichan@yahoo.com>
2023-08-01 14:25:03 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,71 @@
+input.onButtonPressed(Button.A, function () {
+    thisSpeed = 30
+    thisPause = 2000
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, thisSpeed)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, thisSpeed)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor3, Kitronik_Robotics_Board.MotorDirection.Reverse, thisSpeed)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor4, Kitronik_Robotics_Board.MotorDirection.Forward, thisSpeed)
+    basic.pause(thisPause)
+    Kitronik_Robotics_Board.allOff()
+    basic.pause(thisPause)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, thisSpeed)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Reverse, thisSpeed)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor3, Kitronik_Robotics_Board.MotorDirection.Forward, thisSpeed)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor4, Kitronik_Robotics_Board.MotorDirection.Reverse, thisSpeed)
+    basic.pause(thisPause)
+    Kitronik_Robotics_Board.allOff()
+})
+input.onButtonPressed(Button.B, function () {
+    thisSpeed = 40
+    thisPause = 2000
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, thisSpeed)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Reverse, thisSpeed)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor3, Kitronik_Robotics_Board.MotorDirection.Forward, thisSpeed)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor4, Kitronik_Robotics_Board.MotorDirection.Forward, thisSpeed)
+    basic.pause(thisPause)
+    Kitronik_Robotics_Board.allOff()
+    basic.pause(thisPause)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, thisSpeed)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, thisSpeed)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor3, Kitronik_Robotics_Board.MotorDirection.Reverse, thisSpeed)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor4, Kitronik_Robotics_Board.MotorDirection.Reverse, thisSpeed)
+    basic.pause(thisPause)
+    Kitronik_Robotics_Board.allOff()
+})
+radio.onReceivedValue(function (name, value) {
+    thisValue = Math.constrain(value, -100, 100)
+    if (name == "M1") {
+        if (thisValue > 0) {
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, thisValue)
+        } else {
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, -1 * thisValue)
+        }
+    } else if (name == "M2") {
+        if (thisValue > 0) {
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, thisValue)
+        } else {
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Reverse, -1 * thisValue)
+        }
+    } else if (name == "M3") {
+        if (thisValue > 0) {
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor3, Kitronik_Robotics_Board.MotorDirection.Forward, thisValue)
+        } else {
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor3, Kitronik_Robotics_Board.MotorDirection.Reverse, -1 * thisValue)
+        }
+    } else if (name == "M4") {
+        if (thisValue > 0) {
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor4, Kitronik_Robotics_Board.MotorDirection.Forward, thisValue)
+        } else {
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor4, Kitronik_Robotics_Board.MotorDirection.Reverse, -1 * thisValue)
+        }
+    }
+})
+let thisValue = 0
+let thisPause = 0
+let thisSpeed = 0
+basic.showIcon(IconNames.Butterfly)
+radio.setGroup(242)
+Kitronik_Robotics_Board.allOff()
+basic.forever(function () {
+	
+})
